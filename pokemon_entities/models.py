79db790989cfba2ee84c8222ba4baa89946ddce1
alexed34 <alexed34@gmail.com>
2020-04-10 17:49:42 +0400

    1 миграция

---
+++ pokemon_entities/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models import DateTimeField, IntegerField
+
+
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200, default=" ")
+    photo = models.ImageField(upload_to='images', null=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+class PokemonEntity(models.Model):
+    title = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
+    appeared_at = DateTimeField(default='None')
+    disappeared_at = DateTimeField(default='None')
+    level = IntegerField(null=True)
+    health = IntegerField(null=True)
+    strength = IntegerField(null=True)
+    defence = IntegerField(null=True)
+    stamina = IntegerField(null=True)
