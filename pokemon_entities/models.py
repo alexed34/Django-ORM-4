98b276e1ff0b51c196a0a1603e2cea795d7f3efd
alexed34 <alexed34@gmail.com>
2020-04-23 13:57:08 +0400

    32 шаг

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.db.models import DateTimeField, IntegerField
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200,  verbose_name='Имя на русском')
-    title_en = models.CharField(max_length=200,  null=True, verbose_name='Имя на английском')
-    title_jp = models.CharField(max_length=200,  null=True, verbose_name='Имя на японском')
+    title_en = models.CharField(max_length=200,  blank=True, verbose_name='Имя на английском')
+    title_jp = models.CharField(max_length=200,  blank=True, verbose_name='Имя на японском')
     photo = models.ImageField(upload_to='images', null=True, verbose_name='Загрузить фото')
-    description = models.TextField( null=True, verbose_name='Описание')
+    description = models.TextField(default='', blank=True, verbose_name='Описание')
     previous_evolution = models.ForeignKey('self',
                                            verbose_name='Из кого эволюционирует',
                                            null=True,
@@ -20,13 +20,13 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    choose_pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='pokemon_entitys', verbose_name='Выбрать покемона')
+    pokemon_model = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='pokemon_entitys', verbose_name='Выбрать покемона')
     lat = models.FloatField(null=True, verbose_name='Широта')
     lon = models.FloatField(null=True, verbose_name='Долгота')
     appeared_at = DateTimeField(null=True, verbose_name='Время появления')
     disappeared_at = DateTimeField(null=True, verbose_name='Время исчезновения')
-    level = IntegerField(null=True, verbose_name='Уровень')
-    health = IntegerField(null=True, verbose_name='Здоровье')
-    strength = IntegerField(null=True, verbose_name='Сила')
-    defence = IntegerField(null=True, verbose_name='Защита')
-    stamina = IntegerField(null=True, verbose_name='Выносливость')
+    level = IntegerField(default='0', verbose_name='Уровень')
+    health = IntegerField(default='0', verbose_name='Здоровье')
+    strength = IntegerField(default='0', verbose_name='Сила')
+    defence = IntegerField(default='0', verbose_name='Защита')
+    stamina = IntegerField(default='0', verbose_name='Выносливость')
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -23,7 +23,7 @@ def show_all_pokemons(request):
     pokemons = Pokemon.objects.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
-        for pokemon_entity in PokemonEntity.objects.filter(choose_pokemon=pokemon):
+        for pokemon_entity in PokemonEntity.objects.filter(pokemon_model=pokemon):
             add_pokemon(
                 folium_map, pokemon_entity.lat, pokemon_entity.lon,
                 pokemon.title_en,
@@ -46,12 +46,12 @@ def show_pokemons(request, pokemon_id):
     pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
         if pokemon.id == int(pokemon_id):
-            requested_pokemons = PokemonEntity.objects.filter(choose_pokemon=pokemon)
+            requested_pokemons = PokemonEntity.objects.filter(pokemon_model=pokemon)
 
             try:
-                next_evolution = Pokemon.objects.get(previous_evolution=pokemon)
+                next_evolutions = pokemon.next_evolutions.all()
             except:
-                next_evolution = None
+                next_evolutions = None
             break
     else:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
@@ -67,7 +67,7 @@ def show_pokemons(request, pokemon_id):
             pokemon.photo.path,
         )
 
-    pokemons_data_json = []
+    pokemons_data = []
     pokemon_data = {"pokemon_id": pokemon.id,
                     "title_ru": pokemon.title,
                     "title_en": pokemon.title_en,
@@ -81,15 +81,19 @@ def show_pokemons(request, pokemon_id):
                                    "img_url": pokemon.previous_evolution.photo.path}
         pokemon_data['previous_evolution'] = previous_evolution_dict
 
-    if next_evolution:
-        next_evolution_dict = {"title_ru": next_evolution.title,
-                               "pokemon_id": next_evolution.id,
-                               "img_url": next_evolution.photo.path}
-        pokemon_data['next_evolution'] = next_evolution_dict
+    if next_evolutions:
+        next_evolutions_list = []
+        pokemon_data['next_evolutions'] = next_evolutions_list
+        for next_evolution in next_evolutions:
+            next_evolution_dict = {"title_ru": next_evolution.title,
+                                   "pokemon_id": next_evolution.id,
+                                   "img_url": next_evolution.photo.path}
+            next_evolutions_list.append(next_evolution_dict)
 
-    pokemons_data_json.append(pokemon_data)
+    pokemons_data.append(pokemon_data)
 
     return render(request, "pokemon.html", context={'map': folium_map._repr_html_(),
                                                     'pokemon': pokemon,
-                                                    'next_evolution': next_evolution,
+                                                    'next_evolutions': next_evolutions,
                                                     })
+
